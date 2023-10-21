22363b3fc22fa3f566d4eb5f40bc75ca43004a9c
None <120342992+SioNone@users.noreply.github.com>
2023-10-21 20:57:02 +0100

    Fixed Bug
    Fixed player being able to select more controllers than they have connected therefore breaking the game, players now get a warning message when they try this

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,37 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     public static int numPlayers;
 
+    private string[] controllerList;
+
+    [SerializeField]
+    private GameObject controllerWarning;
+
+    [SerializeField]
+    private TMP_Text warningText;
+
     [SerializeField]
     private AudioClip clickSound;
 
+    void Start()
+    {
+        controllerList = Input.GetJoystickNames();
+    }
+
     public void TwoPlayers()
     {
-        numPlayers = 2;
-        StartCoroutine(NextScene());
+        SFXManager.instance.PlaySFX(clickSound, transform, 0.75f);
+
+        if (controllerList.Length == 2)
+        {
+            numPlayers = 2;
+            StartCoroutine(NextScene());
+        }
+        else
+        {
+            PlayWarning(2);
+        }
     }
 
     public void ThreePlayers()
     {
-        numPlayers = 3;
-        StartCoroutine(NextScene());
+        SFXManager.instance.PlaySFX(clickSound, transform, 0.75f);
+
+        if (controllerList.Length == 3)
+        {
+            numPlayers = 3;
+            StartCoroutine(NextScene());
+        }
+        else
+        {
+            PlayWarning(3);
+        }
     }
 
     public void FourPlayers()
     {
-        numPlayers = 4;
-        StartCoroutine(NextScene());
+        SFXManager.instance.PlaySFX(clickSound, transform, 0.75f);
+
+        if (controllerList.Length == 4)
+        {
+            numPlayers = 4;
+            StartCoroutine(NextScene());
+        }
+        else
+        {
+            PlayWarning(4);
+        }
     }
 
     IEnumerator NextScene()
     {
-        SFXManager.instance.PlaySFX(clickSound, transform, 0.75f);
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Game");
+    }
+
+    public void PlayWarning(int numSelected)
+    {
+        controllerWarning.SetActive(true);
+        warningText.text = "Connect " + numSelected.ToString() + " controllers!";
     }
 }
