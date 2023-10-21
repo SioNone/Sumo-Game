482771c406f257b45ea8560cad6509f2be453ea1
None <120342992+SioNone@users.noreply.github.com>
2023-10-21 17:58:57 +0100

    Added Main Menu UI Sounds
    Title

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,23 +7,31 @@ public class PlayerManager : MonoBehaviour
 {
     public static int numPlayers;
 
+    [SerializeField]
+    private AudioClip clickSound;
+
     public void TwoPlayers()
     {
         numPlayers = 2;
+        StartCoroutine(NextScene());
     }
 
     public void ThreePlayers()
     {
         numPlayers = 3;
+        StartCoroutine(NextScene());
     }
 
     public void FourPlayers()
     {
         numPlayers = 4;
+        StartCoroutine(NextScene());
     }
 
-    public void NextScene()
+    IEnumerator NextScene()
     {
+        SFXManager.instance.PlaySFX(clickSound, transform, 0.75f);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Game");
     }
 }
